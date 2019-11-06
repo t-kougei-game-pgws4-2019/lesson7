afcd6faad3d97c53d52bc6f3a37d36f789a78625
kohei <koufuku69@gmail.com>
2019-11-06 09:33:03 +0900

    結果画像とプロジェクトを追加

---
+++ src/Assets/QuadBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera cam = Camera.main;
+
+        transform.parent = cam.transform;
+
+        float distance = cam.farClipPlane - 3.0f;
+        transform.localPosition = new Vector3(0, 0, distance);
+        transform.localRotation = new Quaternion();
+
+        float inv_aspect = (float)Screen.width / (float)Screen.height;
+        float s = Mathf.Tan(0.5f * cam.fieldOfView * Mathf.Deg2Rad) * 2.0f * distance;
+        transform.localScale = new Vector3(inv_aspect * s, s, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
